8b5df6b9fbe44d30aaec3c9974d1aab91c403fb7
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-05-22 17:49:54 +0200

    added a5

---
+++ Aufgabe 5/app/src/main/java/dev/dubsky/Main.java
@@ -0,0 +1,15 @@
+package dev.dubsky;
+
+/**
+ * @author Clemens Maas
+ * @since 2023-05-22
+ */
+public class Main {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
