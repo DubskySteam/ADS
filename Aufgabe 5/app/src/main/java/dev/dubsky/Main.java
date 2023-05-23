8fd12a39434512e951cabb721860129ae4851b74
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-05-23 14:44:32 +0200

    added menu

--- Aufgabe 5/app/src/main/java/dev/dubsky/Main.java
+++ Aufgabe 5/app/src/main/java/dev/dubsky/Main.java
@@ -7,19 +7,7 @@ package dev.dubsky;
 public class Main {
 
     public static void main(String[] args) {
-        Tree t = new Tree();
-        t.insert("F");
-        t.insert("D");
-        t.insert("B");
-        t.insert("E");
-        t.insert("A");
-        t.insert("C");
-        t.insert("J");
-        t.insert("K");
-        t.insert("H");
-        t.insert("G");
-        t.insert("I");
-        t.preOrderTraversal();
+        Menu.getInstance().start();
     }
     
 }
---
+++ Aufgabe 5/app/src/main/java/dev/dubsky/Menu.java
@@ -0,0 +1,70 @@
+package dev.dubsky;
+
+/**
+ * @author Clemens Maas
+ * @since 2023-05-22
+ */
+public class Menu {
+
+    private static Menu instance;
+
+    enum MenuOption {
+        INSERT, SEARCH, PRINT, EXIT
+    }
+
+    public static Menu getInstance() {
+        if (instance == null) {
+            instance = new Menu();
+        }
+        return instance;
+    }
+
+    /**
+     * Prints the menu and returns the selected option.
+     * @return Selected option
+     */
+    public MenuOption getInput() {
+        System.out.println("Choose an option:");
+        System.out.println("1. Insert");
+        System.out.println("2. Search");
+        System.out.println("3. Print");
+        System.out.println("4. Exit");
+        return MenuOption.values()[Integer.parseInt(System.console().readLine()) - 1];
+    }
+
+    /**
+     * Starts the menu.
+     */
+    public void start() {
+        Tree t = new Tree();
+        MenuOption option = MenuOption.INSERT;
+        while (option != MenuOption.EXIT) {
+            option = getInput();
+            switch (option) {
+                case INSERT -> {
+                    System.out.println("Insert a new node:");
+                    t.insert(System.console().readLine());
+                }
+                case SEARCH -> {
+                    System.out.println("Search for a node: ");
+                    Node n = t.search(t.root, System.console().readLine());
+                    if (n != null) {
+                        System.out.println("Found node with value " + n.value);
+                    } else {
+                        System.out.println("Node not found");
+                    }
+                }
+                case PRINT -> {
+                    System.out.println("Printing tree:");
+                    t.preOrderTraversal();
+                }
+                case EXIT -> {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                }
+                default -> throw new IllegalArgumentException("Unexpected value: " + option);
+            }
+        }
+    }
+
+}
